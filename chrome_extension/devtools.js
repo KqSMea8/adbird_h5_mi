d3063a579b3870b29e78bcd97667879324421335
aguangtian <aguangtian@tencent.com>
2018-12-20 10:27:17 +0800

    1

--- chrome_extension/devtools.js
+++ chrome_extension/devtools.js
@@ -1,28 +1,16 @@
-/*
- * Copy Right: Tencent ISUX
- * Comments: web下载插件
- * Author: kundy
- * Date: 2014-12-24
- */
 
-
-
-window.onload=function(){
-  panelInit();
-}
-window.onbeforeunload=function(){
+function panelInit() {
+    chrome.devtools.panels.create("ADBGameSniffer", "icons/logo_48.png", "panel.html",
+        function () {
+            ///TODO
+        }
+    );
 }
 
-
-function panelInit()
-{
-  chrome.devtools.panels.create( "ADBGameSniffer",  "icons/logo_48.png",  "panel.html",
-      function() {
-   
-      }
-  );
+window.onload = function () {
+    panelInit();
 }
 
-
-
-
+window.onbeforeunload = function () {
+    //TODO
+}
